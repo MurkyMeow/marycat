e9fe9366bfd59bd38cd550ea8218dc288d52db9a
MurkyMeow <dinosowermurky@gmail.com>
2019-06-08 21:28:57 +0900

    improve usability of `el` function

---
+++ index2.js
@@ -0,0 +1,61 @@
+function plain($el, str) {
+  const [prefix, rest] = [str[0], str.slice(1)]
+  switch (prefix) {
+    case '.': return $el.classList.add(rest)
+    case '#': return $el.setAttribute('id', rest)
+    case '@': return $el.setAttribute('name', rest)
+    default:
+      return $el.appendChild(document.createTextNode(str))
+  }
+}
+
+const el = name => (...entities) => {
+  const chained = entities
+  return function chain(...entities) {
+    const $parent = entities.find(x => x instanceof Element)
+    if (!$parent) {
+      chained.push(...entities.flat())
+      return chain
+    }
+    const $el = document.createElement(name)
+    chained.forEach(entity => {
+      switch (typeof entity) {
+        case 'string': return plain($el, entity)
+        case 'function': return entity($el, chain)
+        default: throw Error(`Unexpected child: ${entity}`)
+      }
+    })
+    return $parent.appendChild($el)
+  }
+}
+
+function makeState(initial) {
+  let current = initial
+  const observers = []
+  function subscribe(cb) {
+    cb(current)
+    observers.push(cb)
+    return subscribe
+  }
+  Object.defineProperty(subscribe, 'value', {
+    get: () => current,
+    set(value) {
+      current = value
+      observers.forEach(cb => cb(value))
+    }
+  })
+  subscribe.after = f => {
+    const newState = makeState(current)
+    subscribe(value => newState.value = f(value))
+    return newState
+  }
+  return subscribe
+}
+
+const on = name => handler => $el => {
+  $el.addEventListener(name, handler)
+}
+
+const attr = name => value => $el => {
+  $el.setAttribute(name, value)
+}
