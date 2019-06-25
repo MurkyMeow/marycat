64d2daef5dc1157181f1497fdb47d35c1a854094
MurkyMeow <dinosowermurky@gmail.com>
2019-06-26 05:07:44 +0900

    drop previous version, rewrite examples a bit

---
+++ examples/bindings.js
@@ -0,0 +1,11 @@
+const article = el('article')
+const button = el('button')
+const div = el('div')
+const form = el('form')
+const h3 = el('h3')
+const h4 = el('h4')
+const header = el('header')
+const input = el('input')
+const img = el('img')
+
+const click = on('click')
