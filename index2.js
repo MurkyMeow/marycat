c1d9f798707dade683aeff83b7456a9b0633fe58
MurkyMeow <dinosowermurky@gmail.com>
2019-06-26 03:41:31 +0900

    refactor states to make them less memory consuming

--- index2.js
+++ index2.js
@@ -45,55 +45,64 @@ const el = name => chainable(($parent, chained) => {
   return $parent.appendChild($el)
 })
 
-function makeState(initial, params = {}) {
-  let current = initial
-  const observers = []
-  function subscribe(cb) {
-    cb(current)
-    observers.push(cb)
-    return subscribe
-  }
-  Object.defineProperty(subscribe, 'value', {
-    get: () => current,
-    set(value) {
-      if (value === current) return
-      observers.forEach(cb => cb(value, current))
-      current = value
+class State {
+  constructor(initial, params = {}) {
+    const { key, actions = {}, views = {} } = params
+    this.current = initial
+    this.observers = []
+    this.key = key
+    for (const [name, fn] of Object.entries(actions)) {
+      this[name] = (...args) => {
+        this.v = fn(this.v, ...args)
+      }
     }
-  })
-  subscribe.after = f => {
-    const newState = makeState(current)
-    subscribe(value => newState.value = f(value))
-    return newState
+    for (const [name, fn] of Object.entries(views)) {
+      this[name] = this.after(fn)
+    }
   }
-  subscribe.combine = f => state => {
-    const comb = makeState()
-    state(x => comb.value = f(current, x))
-    subscribe(x => comb.value = f(x, state.value))
+  get v() {
+    return this.current
+  }
+  set v(value) {
+    if (value === this.v) return
+    this.observers.forEach(cb => cb(value, this.v))
+    this.current = value
+  }
+  sub(cb) {
+    cb(this.v)
+    this.observers.push(cb)
+    return this
+  }
+  after(f) {
+    const derivation = new State()
+    this.sub(x => derivation.v = f(x))
+    return derivation
+  }
+  not() {
+    return this.after(x => !x)
+  }
+  merge(state, f) {
+    const comb = new State()
+    state.sub(x => comb.v = f(this.v, x))
+    this.sub(x => comb.v = f(x, state.v))
     return comb
   }
-  subscribe.not = () => subscribe.after(x => !x)
-  subscribe.and = subscribe.combine((a, b) => a && b)
-  subscribe.or = subscribe.combine((a, b) => a || b)
-  const { key, actions, views } = params
-  subscribe.key = key
-  for (const [name, fn] of Object.entries(actions || {})) {
-    subscribe[name] = (...args) => {
-      subscribe.value = fn(current, ...args)
-    }
+  and(state) {
+    return this.merge(state, (a, b) => a && b)
   }
-  for (const [name, fn] of Object.entries(views || {})) {
-    subscribe[name] = subscribe.after(fn)
+  or(state) {
+    return this.merge(state, (a, b) => a || b)
   }
-  return subscribe
 }
+
+const makeState = (...args) => new State(...args)
 
 function get(strings, ...keys) {
   return strings.map((str, i) => {
     const state = keys[i]
     if (!state) return str || null
     const text = document.createTextNode('')
-    state(next => text.textContent = str + next)
+    state.sub(next => text.textContent = str + next)
     return $el => $el.appendChild(text)
   })
 }
@@ -114,7 +123,7 @@ const ternary = cond => then => otherwise => $el => {
   const state = cond.after(Boolean)
   const mount = withParent($el)
   let nodes = []
-  state(value => {
+  state.sub(value => {
     while (nodes.length > 1) nodes.pop().remove()
     const $hook = nodes[0] || $el.appendChild(empty())
     const newNodes = mount(value ? then : otherwise)
@@ -128,35 +137,30 @@ const when = cond => chainable(($el, chained) =>
   ternary(cond)(chained)(empty)($el)
 )
 
-function reactiveItem(item) {
-  const observers = []
-  function get(_, prop) {
-    if (prop === '$') return item
-    return cb => {
-      observers.push((value, i) => cb(prop === 'i' ? i : value[prop]))
-    }
-  }
-  function update(value, index) {
-    observers.forEach(cb => cb(value, index))
-  }
-  const getters = new Proxy({}, { get })
-  return { update, getters }
+function reactiveItem(initial) {
+  const state = makeState(initial)
+  const getters = new Proxy({}, {
+    get: (_, prop) => prop === 'v'
+      ? state.v
+      : state.after(x => x[prop]),
+  })
+  return { state, getters }
 }
 
 const iter = state => vnode => $el => {
   let oldLookup = new Map()
   const mount = withParent($el)
   const $hook = $el.appendChild(empty())
-  state((nextState, oldState = []) => {
+  state.sub((nextState, oldState = []) => {
     const newLookup = new Map()
     for (const item of nextState) {
       const key = item[state.key]
       if (oldLookup.has(key)) {
         newLookup.set(key, oldLookup.get(key))
       } else {
-        const { update, getters } = reactiveItem(item)
+        const { state, getters } = reactiveItem(item)
         const $node = mount(vnode(getters))
-        newLookup.set(key, { update, $node })
+        newLookup.set(key, { state, $node })
       }
     }
     for (const item of oldState) {
@@ -170,7 +174,7 @@ const iter = state => vnode => $el => {
       const next = newLookup.get(item[state.key])
       if ($current === next.$node) $current = $current.nextSibling
       else $current.before(next.$node)
-      next.update(item, i)
+      next.state.v = { ...item, i }
     })
     oldLookup = newLookup
   })
