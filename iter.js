1b76f47a353642f106ed9c26184ee7594c990170
MurkyMeow <dinosowermurky@gmail.com>
2019-08-05 02:44:45 +0900

    iter: remove hacky proxy observer

--- iter.js
+++ iter.js
@@ -1,32 +1,25 @@
 import { withParent, empty } from './core.js'
 import { State } from './state.js'
 
-function reactiveItem(initial) {
-  const state = new State(initial)
-  const getters = new Proxy({}, {
-    get: (_, prop) => prop === 'v'
-      ? state.v
-      : state.after(x => x[prop]),
-  })
-  return { state, getters }
-}
-
-export const iter = state => vnode => $el => {
+export const iter = (state, vnode) => $el => {
   let oldLookup = new Map()
   const mount = withParent($el)
   const $hook = $el.appendChild(empty())
   state.sub((nextState, oldState = []) => {
     const newLookup = new Map()
-    for (const item of nextState) {
+    nextState.forEach((item, i) => {
       const key = item[state.key]
       if (oldLookup.has(key)) {
         newLookup.set(key, oldLookup.get(key))
       } else {
-        const { state, getters } = reactiveItem(item)
-        const $node = mount(vnode(getters))
+        const obj = { ...item, i }
+        const state = new State(obj, {
+          observed: Object.keys(obj),
+        })
+        const $node = mount(vnode(state))
         newLookup.set(key, { state, $node })
       }
-    }
+    })
     for (const item of oldState) {
       const key = item[state.key]
       if (!newLookup.has(key)) {
