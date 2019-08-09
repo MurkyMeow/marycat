4d303ecaecb33a20267184151ed0e737b4f1d59d
MurkyMeow <dinosowermurky@gmail.com>
2019-08-09 10:44:26 +0900

    form: add textarea and some more attributes

--- form.js
+++ form.js
@@ -18,13 +18,12 @@ export const form = el('form', {
   },
 })
 
-export const input = el('input', {
-  attrs: [
-    'type', 'value',
-    'placeholder', 'required',
-    'autocomplete', 'autofocus',
-    'disabled', 'readonly',
-  ],
+const fieldAttrs = [
+  'autofocus', 'disabled', 'readonly',
+  'maxlength', 'placeholder', 'required',
+  'autocomplete', 'spellcheck',
+]
+const fieldApi = {
   bind(state) {
     this($el => state.sub(next => $el.value = next))
     this.input(e => state.v = e.target.value)
@@ -34,4 +33,20 @@ export const input = el('input', {
     this($el => state.sub(v => $el.setCustomValidity(v || '')))
     return this
   },
+}
+
+export const input = el('input', {
+  ...fieldApi,
+  attrs: [...fieldAttrs,
+    'type', 'value', 'pattern',
+    'accept', 'multiple', 'list',
+    'max', 'min', 'step',
+  ],
+})
+
+export const textarea = el('textarea', {
+  ...fieldApi,
+  attrs: [...fieldAttrs,
+    'rows', 'cols',
+  ],
 })
