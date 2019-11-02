d7ae3660f4a0334f2901950bb8f6c81671934d18
MurkyMeow <dinosowermurky@gmail.com>
2019-11-03 02:03:33 +0900

    webc: initial implementation

--- src/webc.ts
+++ src/webc.ts
@@ -1,5 +1,5 @@
 import { State, StateOrPlain } from './state'
-import { PipeFn, VirtualNode, attr, pipe, Effect } from './core'
+import { PipeFn, VirtualNode, attr, pipe, Effect, on, dispatch } from './core'
 
 type Props<T> =
   { [key in keyof T]: State<T[key]> }
@@ -15,13 +15,13 @@ export function defAttr<T>(defaultValue: T): State<T> {
   return state
 }
 
-export abstract class MaryElement<T> extends HTMLElement {
+export abstract class MaryElement<T, E> extends HTMLElement {
   root: ShadowRoot = this.attachShadow({ mode: 'open' })
   // as props are defined using default parameters
   // they're considered to be optional and thus nullable
   // `Required` frees from the need of writing null checks
   props: Required<Props<T>>
-  constructor(render: RenderFunction<T>) {
+  constructor(render: RenderFunction<T, E>) {
     super()
     props = {}, keys = []
     const trap = new Proxy({}, {
@@ -30,7 +30,7 @@ export abstract class MaryElement<T> extends HTMLElement {
       },
     })
     const vnode = new VirtualNode<ShadowRoot>(this.root)
-    render(pipe(vnode), trap as Props<T>)
+    render(pipe(vnode), trap as Props<T>, t_dispatch)
     this.props = props as Required<Props<T>>
     const observer = new MutationObserver(m => this.onAttributeChange(m))
     observer.observe(this, {
@@ -54,27 +54,43 @@ export abstract class MaryElement<T> extends HTMLElement {
   }
 }
 
-type RenderFunction<T> =
-  (host: PipeFn<ShadowRoot>, props: Props<T>) => PipeFn<ShadowRoot>
+export type TypedDispatch<E> =
+  <K extends keyof E>(
+    event: K, detail: E[K], opts?: Omit<CustomEventInit, 'detail'>
+  ) => (el: ShadowRoot) => void
 
-export interface CustomElementConstructor<T> {
-  new: (...setup: string[]) => PipeFn<MaryElement<T>>;
+const t_dispatch = <E, K extends keyof E>(
+  event: K, detail: E[K], opts?: Omit<CustomEventInit, 'detail'>,
+) => dispatch(event as string, detail, opts)
+
+type RenderFunction<T, E> =
+  (host: PipeFn<ShadowRoot>, props: Props<T>, t_dispatch: TypedDispatch<E>) => PipeFn<ShadowRoot>
+
+export interface CustomElementConstructor<T, E> {
+  new: (...setup: string[]) => PipeFn<MaryElement<T, E>>
+
+  on: <K extends keyof E>(event: K, handler: (event: CustomEvent<E[K]>) => void) =>
+    (el: MaryElement<T, E>) => void
+
   prop: <K extends keyof T, E extends Node>(key: K, val: StateOrPlain<T[K]>) =>
-    Effect<MaryElement<T>, E>;
+    Effect<MaryElement<T, E>, E>
 }
 
-export const customElement = <T>(
-  elName: string, render: RenderFunction<T>,
-): CustomElementConstructor<T> => {
-  class CustomElement extends MaryElement<T> {
+export const customElement = <T, E>(
+  elName: string, render: RenderFunction<T, E>,
+): CustomElementConstructor<T, E> => {
+  class CustomElement extends MaryElement<T, E> {
     constructor() { super(render) }
   }
   customElements.define(elName, CustomElement)
   return {
-    new(...setup: string[]): PipeFn<MaryElement<T>> {
+    new(...setup: string[]): PipeFn<MaryElement<T, E>> {
       return pipe(new VirtualNode(new CustomElement(), setup))
     },
-    prop: <K extends keyof T>(key: K, val: StateOrPlain<T[K]>) => (el: MaryElement<T>): void => {
+    on<K extends keyof E>(event: K, handler: (event: CustomEvent<E[K]>) => void) {
+      return on(event as string, e => handler(e as CustomEvent<E[K]>))
+    },
+    prop: <K extends keyof T>(key: K, val: StateOrPlain<T[K]>) => (el: MaryElement<T, E>): void => {
       if (typeof val === 'string' || typeof val === 'number' || typeof val === 'boolean') {
         attr(key as string, val)(el)
       } else if (val instanceof State) {
