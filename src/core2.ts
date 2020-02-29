806c84dafcabad4978f81443388165cda1d4f600
MurkyMeow <dinosowermurky@gmail.com>
2020-02-29 23:42:20 +0700

    improve the new api

--- src/core2.ts
+++ src/core2.ts
@@ -1,11 +1,14 @@
-type Effect<TNode extends Node, TChild extends Node, TEvents> =
-  (arg: TNode) => void | TChild
+type VNode<TNode extends Node, TEvents> =
+  (arg: Node) => TNode
+
+type Effect<TNode extends Node, TEvents> =
+  (arg: TNode) => void
 
 type ExtractChild<T> =
-  T extends Effect<Node, infer U, unknown> ? U : never
+  T extends VNode<infer U, unknown>[] ? U : Node
 
 type ExtractEvent<T> =
-  T extends Effect<Node, Node, infer U> ? U : unknown
+  T extends VNode<Node, infer U>[] ? U : unknown
 
 type UnionToIntersection<U> =
   (U extends unknown ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
@@ -13,10 +16,10 @@ type UnionToIntersection<U> =
 const vnode =
   <TNode extends Node, TEvents>(getNode: () => TNode) =>
 
-  <TChildren extends Effect<TNode, Node, unknown>>(
-    children: TChildren[],
-    effects: Effect<TNode, ExtractChild<TChildren>, ExtractEvent<TChildren>>[],
-  ): Effect<Node, TNode, TEvents | ExtractEvent<TChildren>> =>
+  <TChildren extends VNode<Node, unknown>[]>(
+    effects: Effect<TNode, ExtractEvent<TChildren>>[],
+    ...children: TChildren
+  ): VNode<TNode, TEvents & ExtractEvent<TChildren>> =>
 
   (root: Node): TNode => {
     const node = getNode()
@@ -33,28 +36,30 @@ const cx = (classname: string) => (el: Element) => el.classList.add(classname)
 
 const on = <
   TNode extends Node,
-  TChild extends Node,
   TEvents,
   TIntEvents extends UnionToIntersection<TEvents>,
   K extends keyof TIntEvents,
 >(
   event: K,
   handler: (arg: TIntEvents[K] & { currentTarget: TNode }) => void,
-): Effect<TNode, TChild, TEvents> => el => {
+): Effect<TNode, TEvents> => el => {
   // is it possible to prove to the compiler that the right argument is used?
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   el.addEventListener(String(event), handler as any)
 }
 
 div([
-  div([], []),
-  audio([
-    marquee([], []),
-  ], []),
-], [
   cx('app'),
   on('keydown', e => e.key),
   on('ended', e => e.currentTarget),
   on('encrypted', e => e.srcElement), // audio event
   on('bounce', e => e.currentTarget), // marquee event
-])
+],
+  div([],
+    marquee([]),
+    audio([]),
+  ),
+  audio([],
+    marquee([]),
+  ),
+)
